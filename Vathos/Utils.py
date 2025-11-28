d7e452c1a2b170345eaf4c2787e207b87b4777ca
MarioPaerle <paerle.music@gmail.com>
2025-11-28 12:04:30 +0100

    Added an initial _kronecker_batch_matmul function

--- Vathos/Utils.py
+++ Vathos/Utils.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 try:
     from colorama import Fore
+
     GOOD = Fore.GREEN
     BAD = Fore.RED
     RES = Fore.RESET
@@ -11,12 +12,11 @@ try:
     NUM = Fore.BLUE
 except:
     GOOD = ""
-    BAD =  ""
-    RES =  ""
-    SEC =  ""
+    BAD = ""
+    RES = ""
+    SEC = ""
     NUM = ""
     HM = ""
-
 
 FLAG_PASS = 3
 
@@ -60,6 +60,27 @@ def power_weigthed_cumsum(x, a=0.999, rescale=True):
 
 def precompute_power_weigthed_cumsum(x, alpha_pow):
     return torch.cumsum(x / alpha_pow, dim=1) * alpha_pow
+
+
+@torch.compile
+def _kronecker_batch_matmul(A: torch.Tensor, B: torch.Tensor, X: torch.Tensor) -> torch.Tensor:
+    if A.ndim != 2 or B.ndim != 2:
+        raise ValueError("A and B must be 2-D square matrices")
+    if X.ndim != 3:
+        raise ValueError("X must be [B, L, d]")
+    n = A.shape[0]
+    if A.shape[1] != n or B.shape != (n, n):
+        raise ValueError("A and B must be the same square size (n x n)")
+
+    B_batch, L, d = X.shape
+    if L != n * n:
+        raise ValueError(f"L must equal n*n (got L={L}, n*n={n * n})")
+
+    Xm = X.view(B_batch, n, n, d)
+
+    Y = torch.einsum('ij,bjkf,lk->bilf', A, Xm, B)
+
+    return Y.reshape(B_batch, L, d)
 
 
 def flag(text, level=1):
