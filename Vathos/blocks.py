4573cf8e37a0164b69174efc53955aff22127140
MarioPaerle <paerle.music@gmail.com>
2025-12-08 11:29:59 +0100

    weight tying if error

--- Vathos/blocks.py
+++ Vathos/blocks.py
@@ -911,7 +911,8 @@ class SequenceModel(VathosModel):
         self.unembedder = unembedder(d_model, vocab_size)
 
         if weight_tying and isinstance(self.embedder, EasyEmbedder) and isinstance(self.unembedder, UnbiasedLinear):
-            self.unembedder.linear.weight = self.embedder.embeddings.weight
+            self.unembedder.linear.weight = self.embedder.embedding.weight
+
         elif weight_tying:
             raise TypeError("Automatic weight tying is only possible if the embedder is an EasyEmbedder and Unembedder is an UnbiasedLinear."
                             " You shoul manually do weight tying if you aim to use specific layer:"
